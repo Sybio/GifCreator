aa66a57e74e29e70ebad2c226797356c494feeeb
Szabolcs Szász <lunakid@gmail.com>
2014-05-31 09:43:47 +0200

    Moved test stuff to test/.

--- test/index.php
+++ test/index.php
@@ -1,7 +1,7 @@
 <?php
 // Example for creating a slideshow from images in the "./img/" dir.
 
-require "GifCreator/AnimGif.php";
+require "../src/GifCreator/AnimGif.php";
 
 $anim = new GifCreator\AnimGif();
 
@@ -9,7 +9,7 @@ $anim = new GifCreator\AnimGif();
 // Load all images from "./img/", in ascending order,
 // apply the given delays, and save the result...
 
-$anim	-> create("img", array(300, 500)) // first 3s, then 5s for all the others
+$anim	-> create("img/", array(300, 500)) // first 3s, then 5s for all the others
 	-> save("anim.gif");
 
 ?>
